d31cdd7ee37741c350f69131180f0240819ab4ca
Serhii-Zvieriev <zacjok@gmail.com>
2021-07-21 01:11:46 +0300

    60%

--- src/index.js
+++ src/index.js
@@ -3,18 +3,44 @@ import menuListTpl from './templates/menu-list.hbs';
 
 const menuLink = document.querySelector('.js-menu');
 const changeLink = document.querySelector('#theme-switch-toggle');
-
-// console.dir(changeLink.checked = true);
-
-const Theme = {
-    LIGHT: 'light-theme',
-    DARK: 'dark-theme',
-  };
+const bodylink = document.querySelector('body');
 
 menuLink.insertAdjacentHTML('beforeend', createMenuListMarckup(menu));
+
+changeLink.addEventListener('change', changeTheme);
 
 function createMenuListMarckup(menu){
     return menuListTpl(menu);
 }
 
-// console.log(menuListTpl(menu))
+changeTheme ();
+
+function changeTheme () {
+  const themeThis = localStorage.getItem('theme');
+
+  if(!themeThis){
+    if(!changeLink.checked) {
+      localStorage.setItem('theme', 'light-theme');
+    } else {
+      localStorage.setItem('theme', 'dark-theme');
+    }
+
+    themeThis === 'light-theme' ? changeLink.checked = false : changeLink.checked = true;
+  } else {
+    if(changeLink.checked){
+      localStorage.removeItem('theme');
+      localStorage.setItem('theme', 'dark-theme');
+     } else {
+      localStorage.removeItem('theme');
+      localStorage.setItem('theme', 'light-theme');
+    }
+}
+
+  if(!changeLink.checked) {
+    bodylink.classList.remove('dark-theme')
+    bodylink.classList.add('light-theme');
+  } else {
+    bodylink.classList.remove('light-theme')
+    bodylink.classList.add('dark-theme');
+  }
+}
